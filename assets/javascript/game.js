9d4ec29a8a5cf147fd80c49ca1c4ba40b7536a4b
Charles <charlesmbrady@gmail.com>
2019-01-06 19:19:13 -0500

    updated to allow functional reset upon winning and losing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
-///////////////////////////    Sounds   //////////////////////////////////////
+    ///////////////////////////    Sounds   //////////////////////////////////////
     var backgroundMusic = new Audio("https://ia801703.us.archive.org/15/items/StarWarsThemeSongByJohnWilliams/Star%20Wars%20Theme%20Song%20By%20John%20Williams.mp3");
     backgroundMusic.loop = true;
-
     var saberSound = new Audio("./assets/audio/lightsaber1.mp3");
-    var playerDie = new Audio("./assets/audio/pain.mp3");
-    var enemyDie = new Audio("./assets/audio/scream.mp3")
-    
-/////////////////////////////  Characters  ///////////////////////////////////////////
+    var playerDieSound = new Audio("./assets/audio/pain.mp3");
+    var enemyDieSound = new Audio("./assets/audio/scream.mp3");
+
+    /////////////////////////////  Characters  ///////////////////////////////////////////
     var characters = [{
         id: "obi",
         name: "Obi-Wan Kanobi",
@@ -45,222 +44,47 @@ $(document).ready(function () {
         defeated: false
     }];
 
-/////////////////////////// Variables   //////////////////////////////////////////////
+    /////////////////////////// Variables   //////////////////////////////////////////////
     var myCharacter;
     var myCharacterId;
     var myCharacterHp;
     var myCharacterAtk;
     var myCharacterAtkInc;
-    
+
     var defender;
     var defenderId;
     var defenderHp;
     var defenderCtr;
     var defenderName;
-    
+
     var isCharacterChosen = false;
     var isDefenderChosen = false;
 
     var commentary;
     var gameOver = false;
-/******************************************************************************* */
-//dynamically assign character objects icons and output to html document
-for(var i = 0; i < characters.length; i++){
-    var character = $("<div>").addClass("character");
-
-    var name = $("<div>");
-    name.addClass("name");
-    name.text(characters[i].name);
-
-    var image = $("<img>");
-    image.addClass("image");
-    image.attr("src",characters[i].imgsrc);
-
-    var health = $("<div>");
-    health.addClass("health");
-    var healthId = characters[i].id + "health";
-    health.attr("id", healthId);
-    health.text(characters[i].hp);
-
-
-    
-    character.append(name);
-    character.append(image);
-    character.append(health);
-    character.attr("id", characters[i].id);
-    character.attr("data-name", characters[i].name);
-    character.attr("data-atkpwr", characters[i].atkpwr);
-    character.attr("data-atkinc", characters[i].atkpwr);
-    character.attr("data-ctr", characters[i].ctrpwr);
-    character.attr("data-hp", characters[i].hp);
-    $("#characters").append(character);
-}
-
-/////////////////////// Event Listeners //////////////////////////////////////
-$(".character").on("click", function(){
-    console.log("working");
-    if(isCharacterChosen == false){
-        backgroundMusic.play();
-        myCharacter = $(this);
-        myCharacterId = myCharacter.attr("id");
-        myCharacterHp = $(this).data("hp");
-        myCharacterAtk = $(this).data("atkpwr");
-        myCharacterAtkInc = $(this).data("atkinc");
-        
-        
-        
-        $("#my-character").append(myCharacter);
-        isCharacterChosen = true;
-        for(var i = 0; i < characters.length; i++){
-            
-            if(characters[i].id !== this.id){
-                var e = $('#' + characters[i].id);
-                $("#enemies").append(e);
-            }
-        }
-    }
-    if(isDefenderChosen == false){
-        if(this.id == myCharacterId){
-            return 0;
-        }
-        defender = $(this);
-        defenderId = this.id;
-        defenderName = defender.data("name");
-        defenderCtr= defender.data("ctr");
-        defenderHp = defender.data("hp");
-        $("#defender").append(defender);
-        isDefenderChosen = true;
-        
-    }
-    
-
-});
-
-$("#reset").on("click", function(){
-    console.log("working");
-    $("div").remove(".character");
-    $("#reset").css("visibility","hidden");
-    resetGame();
-});
-
-$("#attack-button").on("click",function () {
-    if(myCharacterHp == 0){
-        commentary = "You died :(  Click the RESET button to try again";
-        $("#commentary").text(commentary);
-        return 0;
-    }
-    if(defenderId == null){
-        return 0;
-    }
-
-    defenderHp -= myCharacterAtk;
-    commentary = "You attacked " + defenderName + " for " + myCharacterAtk + " damage!";
-    $("#commentary").text(commentary);
-    saberSound.play();
-    var defTemp = defenderId + "health";
-    var myTemp = myCharacterId + "health";
-
-    
-    $('#' + defTemp).text(defenderHp);
-    if(defenderHp <= 0){
-        defenderHp = 0;
-        enemyDie.play();
-        isDefenderChosen = false;
-        defenderId = null;
-        defenderCtr = null;
-        defenderHp = null;
-        defenderName = null;
-        defender.remove();
-
-    }
-    myCharacterAtk += myCharacterAtkInc;
-    myCharacterHp -= defenderCtr;
-    if(defenderCtr){
-        commentary += " <br> " + defenderName + " attacked you back for " + defenderCtr + " damage!";
-        $("#commentary").html(commentary);
-    }
-
-    if(myCharacterHp <= 0){
-        playerDie.play();
-        alert("You lost all your health.  Click reset to play again.");
-        myCharacterHp = 0;
-            myCharacterAtk = 0;
-            myCharacterAtkInc = 0;
-
-        $("#reset").css("visibility", "visible");
-    }
-    $('#' + myTemp).text(myCharacterHp);
-
-
-    
-
-});
-
-
-
-
-/*****************************************************************************/
-//////////////////////////  Functions   ///////////////////////////////////////////
-
-function setupPage () {         //setup page
-}
-
-function updatePage () {        //update page
-
-}
-
-function selectCharacter () {     //run this when character is selected
-
-}
-
-function attack () {
-
-
-}
-
-function counterAttack () {
-
-}
-
-function resetGame () {
-    isCharacterChosen = false;
-    isDefenderChosen = false;
-
-    gameOver = false;
-
-    myCharacter = null;
-    myCharacterId = null;
-    myCharacterHp = null;
-    myCharacterAtk = null;
-    myCharacterAtkInc = null;
-    
-    defender = null;
-    defenderId = null;
-    defenderHp = null;
-    defenderCtr = null;
-    defenderName = null;
-
-    commentary = null;
-    
-    for(var i = 0; i < characters.length; i++){
+    var wins = 0;
+    var $icons = [];
+    /******************************************************************************* */
+    //dynamically assign character objects icons and output to html document
+    for (var i = 0; i < characters.length; i++) {
         var character = $("<div>").addClass("character");
-    
+
         var name = $("<div>");
         name.addClass("name");
         name.text(characters[i].name);
-    
+
         var image = $("<img>");
         image.addClass("image");
-        image.attr("src",characters[i].imgsrc);
-    
+        image.attr("src", characters[i].imgsrc);
+
         var health = $("<div>");
         health.addClass("health");
         var healthId = characters[i].id + "health";
         health.attr("id", healthId);
         health.text(characters[i].hp);
-    
-    
-        
+
+
+
         character.append(name);
         character.append(image);
         character.append(health);
@@ -271,110 +95,195 @@ function resetGame () {
         character.attr("data-ctr", characters[i].ctrpwr);
         character.attr("data-hp", characters[i].hp);
         $("#characters").append(character);
+        $icons.push(character);
     }
-    
+
     /////////////////////// Event Listeners //////////////////////////////////////
-   $(".character").on("click", function(){
-        if(isCharacterChosen == false){
+    $( "#characters" ).on("click", ".character", function() {
+        console.log("working");
+      });
+    $(".character").on("click", function () {
+        if (gameOver) {
+            alert("game is over reset");
+            return 0;
+        }
+        if (isCharacterChosen == false) {
             backgroundMusic.play();
             myCharacter = $(this);
             myCharacterId = myCharacter.attr("id");
             myCharacterHp = $(this).data("hp");
             myCharacterAtk = $(this).data("atkpwr");
             myCharacterAtkInc = $(this).data("atkinc");
-            
-            
-            
+
+
+
             $("#my-character").append(myCharacter);
             isCharacterChosen = true;
-            for(var i = 0; i < characters.length; i++){
-                
-                if(characters[i].id !== this.id){
+            for (var i = 0; i < characters.length; i++) {
+
+                if (characters[i].id !== this.id) {
                     var e = $('#' + characters[i].id);
                     $("#enemies").append(e);
                 }
             }
         }
-        if(isDefenderChosen == false){
-            if(this.id == myCharacterId){
+        if (isDefenderChosen == false) {
+            if (this.id == myCharacterId) {
                 return 0;
             }
             defender = $(this);
-            defenderId = this.id;
+            defenderId = defender.attr("id");
             defenderName = defender.data("name");
-            defenderCtr= defender.data("ctr");
+            defenderCtr = defender.data("ctr");
             defenderHp = defender.data("hp");
             $("#defender").append(defender);
             isDefenderChosen = true;
-            
+
         }
-        
-    
-    });
-    
-    $("#reset").on("click", function(){
-        console.log("working");
-        $("div").remove(".character");
-        $("#reset").css("visibility","hidden");
+
+
+    }); //end character icon click listener
+
+    $("#reset").on("click", function () {
+        $("#reset").css("visibility", "hidden");
         resetGame();
     });
-    
-    $("#attack-button").on("click",function () {
-        if(myCharacterHp == 0){
+
+    $("#attack-button").on("click", function () {
+        if (myCharacterHp == 0) {
             commentary = "You died :(  Click the RESET button to try again";
             $("#commentary").text(commentary);
             return 0;
         }
-        if(defenderId == null){
+        if (defenderId == null) {
             return 0;
         }
-    
+
+       /* if (gameOver){
+            
+            return 0;
+        }
+        */
+
         defenderHp -= myCharacterAtk;
         commentary = "You attacked " + defenderName + " for " + myCharacterAtk + " damage!";
         $("#commentary").text(commentary);
         saberSound.play();
         var defTemp = defenderId + "health";
         var myTemp = myCharacterId + "health";
-    
-        
+
+
         $('#' + defTemp).text(defenderHp);
-        if(defenderHp <= 0){
-            defenderHp = 0;
-            enemyDie.play();
+        if (defenderHp <= 0) {
+            defenderHp = defender.data("hp");
+            $('#' + defTemp).text(defenderHp);
+            enemyDieSound.play();
+            defender.css("visibility", "hidden");
             isDefenderChosen = false;
             defenderId = null;
             defenderCtr = null;
             defenderHp = null;
-            defender.remove();
-    
+            defenderName = null;
+            wins++;
+            if(wins == (characters.length) - 1){
+                alert("You won! click reset if you want to play again");
+                myCharacterHp = myCharacter.data("hp");
+            $('#' + myTemp).text(myCharacterHp);
+            myCharacterAtk = 0;
+            myCharacterAtkInc = 0;
+            $("#reset").css("visibility", "visible");
+            gameOver = true;
+
+            }
         }
+
         myCharacterAtk += myCharacterAtkInc;
         myCharacterHp -= defenderCtr;
-        if(defenderCtr){
+        if (defenderCtr) {
             commentary += " <br> " + defenderName + " attacked you back for " + defenderCtr + " damage!";
             $("#commentary").html(commentary);
         }
-    
-        if(myCharacterHp <= 0){
-            playerDie.play();
+
+        if (myCharacterHp <= 0) {
+            playerDieSound.play();
             alert("You lost all your health.  Click reset to play again.");
-            myCharacterHp = 0;
+            myCharacterHp = myCharacter.data("hp");
+            $('#' + myTemp).text(myCharacterHp);
             myCharacterAtk = 0;
             myCharacterAtkInc = 0;
-    
+
+            defenderHp = defender.data("hp");
+            $('#' + defTemp).text(defenderHp);
+            isDefenderChosen = false;
+            defenderId = null;
+            defenderCtr = null;
+            defenderHp = null;
+            defenderName = null;
+
             $("#reset").css("visibility", "visible");
+            gameOver = true;
         }
+
         $('#' + myTemp).text(myCharacterHp);
-    
-    
-        
-    
+
+       
+
+
+
+
     });
-    
-
-}
 
 
-/******************************************************************************* */
+
+
+    /*****************************************************************************/
+    //////////////////////////  Functions   ///////////////////////////////////////////
+
+    function setupPage() {         //setup page
+    }
+
+    function updatePage() {        //update page
+
+    }
+
+    function selectCharacter() {     //run this when character is selected
+
+    }
+
+    function attack() {
+
+
+    }
+
+    function counterAttack() {
+
+    }
+
+    function resetGame() {
+        isCharacterChosen = false;
+        isDefenderChosen = false;
+        gameOver = false;
+        wins = 0;
+        commentary = "";
+        $("#commentary").text(commentary);
+
+
+        for (var i = 0; i < $icons.length; i++) {
+            $("#characters").append($icons[i]);
+            $(".character").css("visibility", "visible");
+        }
+
+        //as part of the reset function, 
+        //change their data-attributes back to original
+        //append them to the "characters" section
+        //make commentary blank
+
+
+    }
+
+
+    /******************************************************************************* */
 
 });
+
+
